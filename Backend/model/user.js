25d741d4a634cf034c2dcee22236c6c2bba5d44d
VISAAL24 <visaalm.23ece@kongu.edu>
2025-06-23 19:08:47 +0530

    Day 7

---
+++ Backend/model/user.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    age:Number
+})
+
+const User=mongoose.model('Users',userSchema)
+export default User
